800d28bc5dcabb2822e985948745818ac7723a3f
dhkangRepository <dhkang.repository@gmail.com>
2024-09-12 00:27:32 +0900

    feat : kakao key file

--- src/main/java/com/xmass/cloud/web/controller/RouteController.java
+++ src/main/java/com/xmass/cloud/web/controller/RouteController.java
@@ -1,6 +1,7 @@
 package com.xmass.cloud.web.controller;
 
 import com.xmass.cloud.domain.route.service.RouteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/routes")
+@RequiredArgsConstructor
 public class RouteController {
-    @Autowired
-    private RouteService routeService;
+
+    private final RouteService routeService;
 
     @GetMapping
     public ResponseEntity<String> getRoute(@RequestParam String origin, @RequestParam String destination) {
